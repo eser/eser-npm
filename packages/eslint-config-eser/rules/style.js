1596b65127c8debc9ff67bf1f4902951e28867d7
Eser Ozvataf <eser@ozvataf.com>
2019-10-28 02:47:05 +0300

    Updated import rules.

--- packages/eslint-config-eser/rules/style.js
+++ packages/eslint-config-eser/rules/style.js
@@ -457,10 +457,10 @@ module.exports = {
         'object-curly-newline': [
             'error',
             {
-                ObjectExpression: { minProperties: 4, multiline: true, consistent: true },
-                ObjectPattern: { minProperties: 4, multiline: true, consistent: true },
-                ImportDeclaration: { minProperties: 4, multiline: true, consistent: true },
-                ExportDeclaration: { minProperties: 4, multiline: true, consistent: true },
+                ObjectExpression: { consistent: true },
+                ObjectPattern: { consistent: true },
+                ImportDeclaration: { consistent: true },
+                ExportDeclaration: 'always',
             },
         ],
 
